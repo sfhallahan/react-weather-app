5f64833dcb4adb3cbcc357c959b3029b69340b1b
Sean Hallahan <sfhallahan@gmail.com>
2018-04-22 21:52:02 -0400

    update directiosn

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,13 @@
-var React = require('react');
-var Zipcode = require('./Zipcode');
+var React = require("react");
+var Zipcode = require("./Zipcode");
 
-function Home (props) {
-    return (
-      <div className='home-container'>
-        <h1 className='header'>Enter a City and State</h1>
-        <Zipcode />
-      </div>
-    )
+function Home(props) {
+  return (
+    <div className="home-container">
+      <h1 className="header">Enter a City</h1>
+      <Zipcode />
+    </div>
+  );
 }
-
 
 module.exports = Home;
